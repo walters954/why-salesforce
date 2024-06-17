14aaba7d62b52d18aecdab6fd10cf76dc2ef8733
Jonathan Valoto <jonathan.valoto@gmail.com>
2024-06-17 11:08:29 -0300

    set and show message to user

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 'use strict';
 const tabTemplate = 'tr_template';
 const tabAppendElement = 'tbody';
+const SUCCESS_MESSAGE = 'Your changes were saved successfully';
 loadTabs();
 
 function loadTabs(){
@@ -27,6 +28,8 @@ function addTab(){
     const element = template.content.firstElementChild.cloneNode(true);
     element.querySelector(".delete").addEventListener("click", deleteTab);
     document.querySelector(tabAppendElement).append(element);
+    
+    clearMessage();
 }
 
 function saveTab(){
@@ -56,10 +59,25 @@ function deleteTab(){
 function setChromeStorage(tabs){
     // Save it using the Chrome extension storage API.
     chrome.storage.sync.set({'sfmWhySF': tabs}, function() {
-        //TODO notify user of save
+        setMessage('success', SUCCESS_MESSAGE);
     });
 }
 
+function setMessage(type, message){
+    const messageDiv = document.querySelector("#message");
+    messageDiv.classList.remove('hidden');
+
+    const messageType = document.querySelector("#message-type");
+    messageType.classList.add(`slds-theme_${type}`);
+
+    const messageBody = document.querySelector("#message-body");
+    messageBody.innerText = message;
+}
+
+function clearMessage(){
+    const messageDiv = document.querySelector("#message");
+    messageDiv.classList.add('hidden');
+}
 
 const saveButton = document.querySelector(".save");
 saveButton.addEventListener("click", saveTab);
