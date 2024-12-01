1e0408878380c1e4df60ae9bd362d4885a8aa3d6
Alon Waisman <mowalon@yahoo.com>
2024-11-30 21:34:44 -0700

    Update content.js
    Supports reordering tabs
    Added collapsable Settings area in popup so user can manage options:
    Option to update tabs on save with automatic refresh
    Option to display tabs on front end (instead of just setup)
    Option to set tab background and font colors
    
    Also...
    Increased size of checkboxes so they're easier to click without increasing line height
    Adjusted some code formatting outside of my key changes to improve readability and consistency
    Updated version to 1.6
    
    Note: I refer to the main, non-setup area of Salesforce as "Front End". Honestly, I don't love it, but I don't know what else to call it.

--- content.js
+++ content.js
@@ -1,98 +1,121 @@
-const storageKey = "sfmWhySF";
+const storageKeySettings = "sfmWhySF_settings";
+const storageKeyTabs     = "sfmWhySF_tabs";
+const isSetup            = window.location.pathname.indexOf('/lightning/setup/') != -1;
+let settings;
 
-function init(setupTabUl) {
-    if (setupTabUl) {
-        let rows = [];
-        chrome.storage.sync.get([storageKey], function (items) {
-            let rowObj = items[storageKey] || [];
-            if (rowObj.length === 0) {
-                //Did not find data inside browser storage
-                rowObj = initTabs();
-            }
+runOrSkip();
 
-            for (const rowId in rowObj) {
-                let row = rowObj[rowId];
-                rows.push(
-                    generateRowTemplate(row.tabTitle, row.url, row.openInNewTab)
-                );
-            }
-            setupTabUl.insertAdjacentHTML("beforeend", rows.join(""));
+function runOrSkip() {
+    chrome.storage.sync.get([storageKeySettings], function (items) {
+        settings = items[storageKeySettings];
+        Object.defineProperty(settings, 'displayOnTop', { get() {return !isSetup || this.tabsOnTopInSetup;} });
 
-            // Add click event listeners after rows are inserted
-            addClickEventListeners();
-        });
-    }
-}
+        // console.log('SETTINGS: ', settings);
 
-function delayLoadSetupTabs(count) {
-    const setupTabUl = document.getElementsByClassName(
-        "tabBarItems slds-grid"
-    )[0];
-    count++;
+        let run = (isSetup && settings.showInSetup) || (!isSetup && settings.showInFront);
 
-    if (count > 5) {
-        console.log("Why Salesforce - failed to find setup tab.");
-        return;
-    }
-
-    if (!setupTabUl) {
-        setTimeout(function () {
-            delayLoadSetupTabs(count);
-        }, 3000); // Fixed to pass count correctly
-    } else {
-        init(setupTabUl);
-    }
-}
-
-setTimeout(function () {
-    delayLoadSetupTabs(0);
-}, 3000);
-
-function generateRowTemplate(tabTitle, url, openInNewTab) {
-    const target = openInNewTab ? "_blank" : "_self";
-    return `<li role="presentation" class="oneConsoleTabItem tabItem slds-context-bar__item borderRight navexConsoleTabItem" data-aura-class="navexConsoleTabItem" data-url="${url}">
-            <a role="tab" tabindex="-1" title="${tabTitle}" aria-selected="false" href="${url}" target="${target}" class="tabHeader slds-context-bar__label-action">
-                <span class="title slds-truncate">${tabTitle}</span>
-            </a>
-        </li>`;
-}
-
-function initTabs() {
-    let tabs = [
-        { tabTitle: "Home", url: "/", openInNewTab: false },
-        {
-            tabTitle: "Flow",
-            url: "/lightning/setup/Flows/home",
-            openInNewTab: true,
-        },
-        {
-            tabTitle: "User",
-            url: "/lightning/setup/ManageUsers/home",
-            openInNewTab: false,
-        },
-    ];
-
-    chrome.storage.sync.set({ sfmWhySF: tabs }, function () {
-        //TODO combine with popup.js with background service
-    });
-    return tabs;
-}
-
-function addClickEventListeners() {
-    chrome.storage.sync.get([storageKey], function (items) {
-        const rowObj = items[storageKey] || [];
-        for (const rowId in rowObj) {
-            let tab = rowObj[rowId];
-            document
-                .querySelectorAll(`a[href="${tab.url}"]`)
-                .forEach((link) => {
-                    link.addEventListener("click", function (event) {
-                        if (tab.openInNewTab) {
-                            event.preventDefault();
-                            window.open(tab.url, "_blank");
-                        }
-                    });
-                });
+        if (run) {
+            setTimeout(function () {
+                delayLoadSetupTabs(0);
+            }, 1000);
         }
     });
 }
+
+function delayLoadSetupTabs(count) {
+    const insertionElement = settings.displayOnTop ? document.getElementById('oneHeader') : document.getElementsByClassName("tabBarItems slds-grid")[0];
+    count++;
+
+    if (count > 5) {
+        console.log("Why Salesforce - failed to find insertion point.");
+        return;
+    }
+
+    if (!insertionElement) {
+        setTimeout(function () {
+            delayLoadSetupTabs(count);
+        }, 3000);
+    } else {
+        init(insertionElement);
+    }
+}
+    function init(insertionElement) {
+        if (insertionElement) {
+            let tabs = [];
+            chrome.storage.sync.get([storageKeyTabs], function (items) {
+                let tabsData = items[storageKeyTabs] || [];
+
+                if (tabsData.length === 0) {
+                    //Did not find data inside browser storage
+                    tabsData = initTabs();
+                }
+
+                tabsData.forEach(tabData => {
+                    tabs.push(
+                        generateRowTemplate(tabData.tabTitle, tabData.url, tabData.openInNewTab)
+                    );
+                });
+
+                let insertionPosition = settings.displayOnTop ? "afterbegin" : "beforeend";
+                insertionElement.insertAdjacentHTML(insertionPosition, content(tabs));
+
+                // Add click event listeners after rows are inserted
+                addClickEventListeners();
+            });
+        }
+    }
+        function initTabs() {
+            let tabs = [
+                { tabTitle: "Home", url: "/", openInNewTab: false },
+                {
+                    tabTitle: "Flow",
+                    url: "/lightning/setup/Flows/home",
+                    openInNewTab: true,
+                },
+                {
+                    tabTitle: "User",
+                    url: "/lightning/setup/ManageUsers/home",
+                    openInNewTab: false,
+                },
+            ];
+
+            chrome.storage.sync.set({ sfmWhySF_tabs: tabs }, function () {
+                //TODO combine with popup.js with background service
+            });
+            return tabs;
+        }
+        function generateRowTemplate(tabTitle, url, openInNewTab) {
+            const target = openInNewTab ? "_blank" : "_self";
+
+            return `<li role="presentation" class="oneConsoleTabItem tabItem slds-context-bar__item borderRight navexConsoleTabItem" style="height:36px" data-aura-class="navexConsoleTabItem" data-url="${url}">
+                        <a role="tab" tabindex="-1" title="${tabTitle}" aria-selected="false" href="${url}" target="${target}" class="tabHeader slds-context-bar__label-action">
+                            <span class="title slds-truncate">${tabTitle}</span>
+                        </a>
+                    </li>`;
+        }
+        function content(tabs) {
+            let allTabs = tabs.join("");
+            let styles = [`background-color:${settings.backgroundColor || 'white'}`,
+                          'border-bottom:1px solid lightgray',
+                          `color:${settings.fontColor || 'black'}`];
+
+            return !settings.displayOnTop ? allTabs : `<ul class="slds-grid" style="${styles.join(';')}">${allTabs}</ul>`;
+        }
+        function addClickEventListeners() {
+            chrome.storage.sync.get([storageKeyTabs], function (items) {
+                const rowObj = items[storageKeyTabs] || [];
+                for (const rowId in rowObj) {
+                    let tab = rowObj[rowId];
+                    document
+                        .querySelectorAll(`a[href="${tab.url}"]`)
+                        .forEach((link) => {
+                            link.addEventListener("click", function (event) {
+                                if (tab.openInNewTab) {
+                                    event.preventDefault();
+                                    window.open(tab.url, "_blank");
+                                }
+                            });
+                        });
+                }
+            });
+        }
