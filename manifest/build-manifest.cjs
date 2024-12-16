d54a36378ab96244e727351ebd6e8dbc67c2c6ed
Alfredo <a.chissotti@reply.it>
2024-12-16 15:00:29 +0100

    running fmt and removed package.json

--- manifest/build-manifest.cjs
+++ manifest/build-manifest.cjs
@@ -3,21 +3,18 @@ const manifest = require("./template-manifest");
 
 const browser = process.argv[2];
 if (browser === "firefox") {
-    const background_script = manifest.background.service_worker;
-    manifest.background.scripts = [background_script];
+	const background_script = manifest.background.service_worker;
+	manifest.background.scripts = [background_script];
 
-    delete manifest.minimum_chrome_version;
-    delete manifest.background.service_worker;
-}
-else if (browser === "chrome") {
-    delete manifest.browser_specific_settings;
-}
-else if (browser === "safari") {
-    delete manifest.browser_specific_settings;
-}
-else {
-    console.log(`Usage: ${process.argv[0]} ${process.argv[1]} (firefox || chrome || safari)`);
-    throw new Error(`Unknown browser: ${browser}`);
+	delete manifest.minimum_chrome_version;
+	delete manifest.background.service_worker;
+} else if (browser === "chrome") {
+	delete manifest.browser_specific_settings;
+} else if (browser === "safari") {
+	delete manifest.browser_specific_settings;
+} else {
+	console.error(`Usage: ${process.argv[0]} ${process.argv[1]} (firefox || chrome || safari)`);
+	throw new Error(`Unknown browser: ${browser}`);
 }
 
 const fs = require("fs");
